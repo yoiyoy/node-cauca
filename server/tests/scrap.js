1f7b1defaead283d60b81a93500a37f4870e1c1b
Jaeyun <jaeyunchoe@yoiyoy.com>
2017-01-31 14:00:16 +0900

    add a description

--- server/tests/scrap.js
+++ server/tests/scrap.js
@@ -1,18 +1,19 @@
+/* eslint-env  mocha */
 import chai from 'chai';
 import scrap from '../src/scrap';
+import sidos from '../src/props/sidoList.json';
 
 const expect = chai.expect;
 const handleErr = (err, done, cb) => {
-  if(!err && cb) cb();
+  if (!err && cb) cb();
   done(err);
 };
 
 describe('SCRAP', () => {
-
   let scrappedItems;
   beforeEach((done) => {
     scrap
-      .then(items => {
+      .then((items) => {
         scrappedItems = items;
         done();
       })
@@ -22,8 +23,20 @@ describe('SCRAP', () => {
   describe('scrapped item', () => {
     it('should be a array', (done) => {
       expect(scrappedItems).to.be.a('array');
-      done()
+      done();
+    });
+
+    it('should have items per sido', (done) => {
+      sidos.forEach((sido, i) => {
+        expect(scrappedItems[i]).to.have.property('sidoName')
+        .that.is.a('string')
+        .that.is.equals(sido.name);
+
+        expect(scrappedItems[i]).to.have.property('sidoCode')
+        .that.is.a('number')
+        .that.is.equals(sido.code);
+      });
+      done();
     });
   });
-
 });
