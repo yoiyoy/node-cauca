c6756e6ab5b39156a6cd9bf1d0c77ad3f4d5ed09
yoiyoy <jaeyunchoe@yoiyoy.com>
2017-02-05 23:47:28 +0900

    비동기 요청간 일정한 시간 간격을 보장하는 큐 사용
    mapSerial로 작성한 코드 변경 :  promise를 반환하는 send 함수의 배열을 yield 해도 병렬로 처리되지 않는다.

--- server/src/scrap.js
+++ server/src/scrap.js
@@ -1,32 +1,20 @@
 import co from 'co';
-import wait from 'co-wait';
+
 import request from 'co-request';
 import cheerio from 'cheerio';
 import iconv from 'iconv-lite';
 
+import requestQueue from './utils/request-queue';
 import cheerioInnerText from './utils/cheerio-innerText';
 import parsers from './parsers';
 import { items as itemsRequest, sigu as siguRequest } from './props/requestOptions.json';
 import addrCode from './props/addrCode.json';
 
-// 무조건 기다리는 시간, 요청간 일정한 시간 간격을 보장하지 않음
-const MIN_DELAY_MS = 2000;
+const MIN_INTERVAL = 10000;
+
 cheerioInnerText(cheerio);
 
-// array를 비동기지만 순차적으로 map을 수행함
-// coFunc는 yieldable을 반환 하는 함수
-const mapSerial = (array, coFunc) => co(function* () {
-  const resultArray = [];
-  const len = array.length;
-  for (let i = 0; i < len; i += 1) {
-    const result = yield coFunc(array[i], i, array);
-    resultArray.push(result);
-  }
-  return resultArray;
-});
-
 const send = requestOption => co(function* () {
-  yield wait(MIN_DELAY_MS);
   const response = yield request(requestOption);
   const bodyBuffer = response.body;
   if (response.statusCode !== 200) {
@@ -34,6 +22,11 @@ const send = requestOption => co(function* () {
   }
   const body = iconv.decode(bodyBuffer, 'EUC-KR').toString();
   return cheerio.load(body, { decodeEntities: false });
+});
+
+const requestQ = requestQueue(send, {
+  minInterval: MIN_INTERVAL,
+  asyncWithInterval: true, // 비동기 요청간 일정한 시간 간격을 보장
 });
 
 const getSidoSigu = sidoCode => co(function* () {
@@ -44,22 +37,21 @@ const getSidoSigu = sidoCode => co(function* () {
     },
     encoding: null,
   };
-  const $ = yield send(sidoSiguRequest);
+  const $ = yield requestQ.send(sidoSiguRequest);
   const { siguParser } = parsers($);
   const sidoSigus = $('xsync > select > option:not([value=""])').get().map(siguParser);
   return sidoSigus;
 });
 
-const getAddrCode = sidos => co(function* () {
-  const sidoSigus = yield mapSerial(sidos, sido => co(function* () {
+const getAddrCode = sidos =>
+  sidos.map(sido => co(function* () {
     const sigus = yield getSidoSigu(sido.code);
+    // console.log(sigus);
     return {
       ...sido,
       sigus,
     };
   }));
-  return sidoSigus;
-});
 
 const getSidoSiguItems = (sidoCode, siguCode) => co(function* () {
   const sidoItemsRequest = { ...itemsRequest,
@@ -72,30 +64,26 @@ const getSidoSiguItems = (sidoCode, siguCode) => co(function* () {
     },
     encoding: null,
   };
-  const $ = yield send(sidoItemsRequest);
+  const $ = yield requestQ.send(sidoItemsRequest);
   const { itemParser } = parsers($);
   const sidoItems = $('.Ltbl_list tbody tr').get().map(itemParser);
-
+  // console.log(sidoItems);
   return sidoItems;
 });
 
 const getItems = sidoSigus => co(function* () {
-  yield mapSerial(sidoSigus, sido => co(function* () {
-    yield mapSerial(sido.sigus, sigu => co(function* () {
-      const items = yield getSidoSiguItems(sido.code, sigu.code);
-      return {
-        sidoName: sido.name,
-        sidoCode: sido.code,
-        siguName: sigu.name,
-        siguCode: sigu.code,
-        items,
-      };
-    }));
-  }));
+  yield sidoSigus.map(sido =>
+    sido.sigus.map(sigu => ({
+      sidoName: sido.name,
+      sidoCode: sido.code,
+      siguName: sigu.name,
+      siguCode: sigu.code,
+      items: getSidoSiguItems(sido.code, sigu.code),
+    })));
 });
 
 export default co(function* () {
-  const sidoSigus = yield getAddrCode(addrCode.sidos);
-  const items = yield getItems(sidoSigus.slice(1, 1));
+  const sidoSigus = yield getAddrCode(addrCode.sidos.slice(1, 4));
+  const items = yield getItems(sidoSigus.slice(1, 2));
   return items;
 }).catch(e => console.error(e));
